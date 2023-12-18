becd53328891f55e54d9c68861220c2a8ff9938c
Fish_In_A_Suit <skorjanc.aljosa@gmail.com>
2023-12-18 18:54:22 +0100

    NoneType bugfix in GOTerm.py
    Association processing in GOTerm would throw NoneType if there is an http error when querying associations.

--- src/goreverselookup/core/GOTerm.py
+++ src/goreverselookup/core/GOTerm.py
@@ -311,7 +311,8 @@ class GOTerm:
         products_taxa_dict = {}
         _d_unique_dbs = set() # unique databases of associations; eg. list of all unique assoc['subject']['id']
         
-        for assoc in data['associations']:
+        associations = data.get('associations', [])
+        for assoc in associations:
             # if evidence is not confirmed, continue to next iteration
             evidence_confirmed, evidence_code_eco_id = GOApi.check_GO_association_evidence_code_validity(assoc, model_settings.valid_evidence_codes)
             if not evidence_confirmed:
